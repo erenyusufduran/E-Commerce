abc07a3bf5dee7ccb15f0a0727bf0cf32210187e
Eren Yusuf Duran <erenydurann@gmail.com>
2022-03-11 01:49:21 +0300

    Some SCSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component.jsx";
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
